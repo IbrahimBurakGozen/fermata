c9bbc80765673bbc06578f687dc611c1eab88e09
PimTournaye <pim.tournaye@student.ehb.be>
2021-10-17 16:00:51 +0200

    Added another key but not yet in the database, just to be safe

--- src/logic/Key.ts
+++ src/logic/Key.ts
@@ -6,7 +6,7 @@ class Key{
 	/* Musical keys, stored in Arrays. */
     public C_MAJOR: Array<string> = [ "C Major",  "C1", "D1", "E1", "F1", "G1", "A1", "B1", "C2", "D2", "E2", "F2", "G2", "A2", "B2", "C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4"];
     public C_MINOR: Array<string> = [ "C Minor",  "C1", "D1", "D#1", "F1", "G1", "G#1", "A#1", "C2", "D2", "D#2", "F2", "G2", "G#2", "A#2", "C3", "D3", "D#3", "F3", "G3", "G#3", "A#3", "C4", "D4", "D#4", "F4", "G4", "G#4", "A#4"];
-
+	public WHOLETONE: Array<string> = ["Wholetone", "C1", "D1", "E1", "F#1", "G#1", "A#1", "C2", "D2", "E2", "F#2", "G#2", "A#2", "C3", "D3", "E3", "F#3", "G#3", "A#3", "C4"]
 	/** Array of all the keys. */
     public DATABASE: Array<Array<string>> = [this.C_MAJOR, this.C_MINOR];
 	/** Number used with key array to select and identify the current key. */
