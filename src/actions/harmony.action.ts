cfe147fd99b6cc1b10a84532da2ec619079a2677
PimTournaye <pim.tournaye@student.ehb.be>
2021-10-17 11:48:52 +0200

    Converted if else to switch statement

--- src/actions/harmony.action.ts
+++ src/actions/harmony.action.ts
@@ -16,33 +16,81 @@ class Harmony extends Action{
 		let choices: Array<any> = [];
 		var i/* :Map<String, String> */ = Intervals.loadout;
 
-		if (Note.lastAbsolute == i.get("one1")) choices = [i.get("thr1"), i.get("fiv1"), i.get("thr2"), i.get("fiv2")];
-		else if (Note.lastAbsolute == i.get("two1")) choices = [i.get("fiv1"), i.get("sev1"), i.get("fiv2")];
-		else if (Note.lastAbsolute == i.get("thr1")) choices = [i.get("fiv1"), i.get("one2")];
-		else if (Note.lastAbsolute == i.get("for1")) choices = [i.get("fiv1"), i.get("one2"), i.get("two2")];
-		else if (Note.lastAbsolute == i.get("fiv1")) choices = [i.get("thr1"), i.get("sev1"), i.get("thr2")];
-		else if (Note.lastAbsolute == i.get("six1")) choices = [i.get("one2"), i.get("thr2")];
-		else if (Note.lastAbsolute == i.get("sev1")) choices = [i.get("thr1"), i.get("fiv1"), i.get("thr2")];
-		else if (Note.lastAbsolute == i.get("one2")) choices = [i.get("fiv1"), i.get("thr2"), i.get("fiv2")];
-		else if (Note.lastAbsolute == i.get("two2")) choices = [i.get("fiv1"), i.get("fiv2"), i.get("sev2")];
-		else if (Note.lastAbsolute == i.get("thr2")) choices = [i.get("sev1"), i.get("one2"), i.get("fiv2"), i.get("sev2"), i.get("one3")];
-		else if (Note.lastAbsolute == i.get("for2")) choices = [i.get("two2"), i.get("fiv2"), i.get("one3")];
-		else if (Note.lastAbsolute == i.get("fiv2")) choices = [i.get("thr2"), i.get("sev2"), i.get("thr3")];
-		else if (Note.lastAbsolute == i.get("six2")) choices = [i.get("one3"), i.get("thr3")];
-		else if (Note.lastAbsolute == i.get("sev2")) choices = [i.get("thr2"), i.get("fiv2"), i.get("thr3")];
-		else if (Note.lastAbsolute == i.get("one3")) choices = [i.get("thr2"), i.get("fiv2"), i.get("thr3"), i.get("fiv3")];
-		else if (Note.lastAbsolute == i.get("two3")) choices = [i.get("fiv2"), i.get("sev2"), i.get("fiv3")];
-		else if (Note.lastAbsolute == i.get("thr3")) choices = [i.get("sev2"), i.get("one3"), i.get("fiv3"), i.get("sev3"), i.get("one4")];
-		else if (Note.lastAbsolute == i.get("for3")) choices = [i.get("two3"), i.get("fiv3"), i.get("one4")];
-		else if (Note.lastAbsolute == i.get("fiv3")) choices = [i.get("thr3"), i.get("sev3"), i.get("one4")];
-		else if (Note.lastAbsolute == i.get("six3")) choices = [i.get("thr3"), i.get("one4")];
-		else if (Note.lastAbsolute == i.get("sev3")) choices = [i.get("thr3"), i.get("fiv3")];
-		else if (Note.lastAbsolute == i.get("one4")) choices = [i.get("thr3"), i.get("fiv3")];
+		switch (Note.lastAbsolute) {
+			case i.get("one1"):
+				choices = [i.get("thr1"), i.get("fiv1"), i.get("thr2"), i.get("fiv2")];
+				break;
+			case i.get("two1"):
+				choices = [i.get("fiv1"), i.get("sev1"), i.get("fiv2")];
+				break;
+			case i.get("thr1"):
+				choices = [i.get("fiv1"), i.get("one2")];
+				break;
+			case i.get("for1"):
+				choices = [i.get("fiv1"), i.get("one2"), i.get("two2")];
+				break;
+			case i.get("fiv1"):
+				choices = [i.get("thr1"), i.get("sev1"), i.get("thr2")];
+				break;
+			case i.get("six1"):
+				choices = [i.get("one2"), i.get("thr2")];
+				break;
+			case i.get("sev1"):
+				choices = [i.get("thr1"), i.get("fiv1"), i.get("thr2")];
+				break;
+			case i.get("one2"):
+				choices = [i.get("fiv1"), i.get("thr2"), i.get("fiv2")];
+				break;
+			case i.get("two2"):
+				choices = [i.get("fiv1"), i.get("fiv2"), i.get("sev2")];
+				break;
+			case i.get("thr2"):
+				choices = [i.get("sev1"), i.get("one2"), i.get("fiv2"), i.get("sev2"), i.get("one3")];
+				break;
+			case i.get("for2"):
+				choices = [i.get("two2"), i.get("fiv2"), i.get("one3")];
+				break;
+			case i.get("fiv2"):
+				choices = [i.get("thr2"), i.get("sev2"), i.get("thr3")];
+				break;
+			case i.get("six2"):
+				choices = [i.get("one3"), i.get("thr3")];
+				break;
+			case i.get("sev2"):
+				choices = [i.get("thr2"), i.get("fiv2"), i.get("thr3")];
+				break;
+			case i.get("one3"):
+				choices = [i.get("thr2"), i.get("fiv2"), i.get("thr3"), i.get("fiv3")];
+				break;
+			case i.get("two3"):
+				choices = [i.get("fiv2"), i.get("sev2"), i.get("fiv3")];
+				break;
+			case i.get("thr3"):
+				choices = [i.get("sev2"), i.get("one3"), i.get("fiv3"), i.get("sev3"), i.get("one4")];
+				break;
+			case i.get("for3"):
+				choices = [i.get("two3"), i.get("fiv3"), i.get("one4")];
+				break;
+			case i.get("fiv3"):
+				choices = [i.get("thr3"), i.get("sev3"), i.get("one4")];
+				break;
+			case i.get("six3"):
+				choices = [i.get("thr3"), i.get("one4")];
+				break;
+			case i.get("sev3"):
+				choices = [i.get("thr3"), i.get("fiv3")];
+				break;
+			case i.get("one4"):
+				choices = [i.get("thr3"), i.get("fiv3")];
+				break;
+		}
 
 		harmonyTone = Math.floor(Math.random() * choices.length);
+		console.log("Harmony tone => ", harmonyTone);
 
 		let note = new Sound(`/home/ubuntu/fermata/src/actions/sounds/${harmonyTone}.wav`)
 		await note.play();
+
 		Note.lastHarmony = harmonyTone;
 	}
 
