a941611f41bf1840575108097c744e437eeeee5d
PimTournaye <pim.tournaye@student.ehb.be>
2021-10-17 16:15:42 +0200

    doubled down on transpose.onPress()

--- src/actions/octave.action.ts
+++ src/actions/octave.action.ts
@@ -17,10 +17,11 @@ class Octave extends Action{
 
         for (let i = 0; i < note.DATABASE.length; i++) {
             if (note.lastAbsolute == note.DATABASE[i]) {
-                console.log('playing octave tone')
 				while (octaveTone == "" || octaveTone == null) octaveTone = note.DATABASE[i + octaveDownorUp[Math.floor(Math.random() * octaveDownorUp.length)]];
 			}
         }
+    console.log("Octave tone => ", octaveTone);
+    
 	let playthis = new Sound(`/home/ubuntu/fermata/src/actions/sounds/${octaveTone}.wav`)
     await playthis.play();
     note.lastOctave = octaveTone;
--- src/actions/transpose.action.ts
+++ src/actions/transpose.action.ts
@@ -1,14 +1,18 @@
 import Action from "./action";
 import Key from "../logic/Key";
 import mode  from "../logic/Mode";
+import harmony from "./harmony.action";
 
-class Transpose extends Action{
+class Transpose extends Action {
+
     onPress(): void {
         mode.change();
         console.log('changing key in transpose');
-        
         Key.change();
+        this.generateNote();
+        harmony.playHarmonyTone();
         this.playChord();
+        
     }
 
     public toString(): string {
